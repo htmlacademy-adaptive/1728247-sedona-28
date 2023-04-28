a128c6f8b1782607b53aa259c196047097a871ac
MarieKrymova <lifeandfreedom@gmail.com>
2023-04-29 00:41:10 +0300

    добавлено основное оформление (без автоматизации)

---
+++ source/js/script.js
@@ -0,0 +1,17 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+let map = document.querySelector(".sedona-map");
+map.classList.remove("sedona-map--nojs");

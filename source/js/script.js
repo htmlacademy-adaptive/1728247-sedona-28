8433046cec5915990a24af92c5995b83cadf6b35
MarieKrymova <lifeandfreedom@gmail.com>
2023-05-24 20:36:48 +0300

    corrections map & modal

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,4 @@
+// Navigation menu
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 
@@ -13,5 +14,42 @@ navToggle.addEventListener('click', function () {
   }
 });
 
+// Sedona-map
 let map = document.querySelector(".sedona-map");
 map.classList.remove("sedona-map--nojs");
+
+// Form
+/*
+let form = document.querySelector('.feedback');
+
+if (form) {
+let submit = document.querySelector('.form__button');
+let confirmButtonOk = document.querySelector('.window__button--close');
+let confirmButtonError = document.querySelector('.window__button--ok');
+let modalOk = document.querySelector('.modal--success');
+let modalError = document.querySelector('.modal--error');
+let modalActive = document.querySelector('.modal--opened');
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
+submit.addEventListener('click', function () {
+  let rndNum = randomIntFromInterval(1, 100);
+  if (rndNum<51) {
+    modalOk.classList.add('modal--opened');
+    console.log('success')
+  } else {
+    modalError.classList.add('modal--opened');
+    console.log('error')
+  }
+});
+
+confirmButtonOk.addEventListener('click', function () {
+  modalOk.classList.remove('modal--opened');
+});
+
+confirmButtonError.addEventListener('click', function () {
+  modalError.classList.remove('modal--opened');
+});
+}*/
